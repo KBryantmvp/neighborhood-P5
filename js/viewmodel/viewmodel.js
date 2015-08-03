866fc9035f39aea041ec103c019c80e148364e35
da990s <dal1986@gmail.com>
2015-08-03 14:13:09 -0500

    Trying to make an Ajax request to Wikipedia

--- js/viewmodel/viewmodel.js
+++ js/viewmodel/viewmodel.js
@@ -17,9 +17,44 @@ var ListViewModel = function() {
     };
 
     self.infoWindow = ko.observable(myNeighborhood.infoWindow);
+    var infoWindowContent = "";
+
+    
+        // wikiData(marker);
+    function doAnAjax(marker) {
+        $.ajax({
+            url: 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=1&search=' + marker.title,
+            // url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json',
+            dataType: 'jsonp',
+            // headers: { 'Api-User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36' },
+            success: showWikiData,
+            error: function(data2) {
+                console.log('error: ' + data2);
+            }
+        });
+    }
+
+    var showWikiData = function(data) {
+        console.log('this is my data: ' + data);
+        var wikiTitle = data[1];
+        var wikiSnippet = data[2];
+        var wikiLink = data[3];
+        if (wikiLink.length !== 0) {
+            console.log(wikiLink[0]);
+            infoWindowContent = '<a href="' + wikiLink + '">' + wikiTitle + '</a><br>' + '<p>' + wikiSnippet + '</p>' +
+            '<br><a href="' + wikiLink + '">' + wikiLink + '</a>';
+            console.log(infoWindowContent);
+        } else {
+            console.log('wikiTitle: ' + data[0]);
+            infoWindowContent = '<p>' + data[0] + '</p>' + '<br>' + '<p>No Wikipedia articles were found</p>';
+        }
+        // showInfoWindow(infoWindowContent);
+    };
 
     self.showInfoWindow = function(marker) {
-        self.infoWindow().setContent(marker.title);
+        doAnAjax(marker);
+        self.infoWindow().setContent(infoWindowContent);
+        console.log(infoWindowContent);
         self.infoWindow().open(myNeighborhood.map, marker);
     };
 
@@ -35,14 +70,14 @@ var ListViewModel = function() {
                 // console.log(self.markers()[i].visible());
                 self.markers()[i].showItem(true);
                 self.markers()[i].setVisible(true);
-                console.log(self.markers()[i]);
+                // myNeighborhood.map.setCenter(self.markers()[i].getPosition());
                 // self.showItem(true);
                 // console.log(i);
             } else {
-                console.log('else:');
                 // console.log(self.markers()[i].visible());
                 self.markers()[i].showItem(false);
                 self.markers()[i].setVisible(false);
+                self.infoWindow().close();
                 // self.showItem(false);
             }
         }
